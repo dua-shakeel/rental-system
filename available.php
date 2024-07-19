cf5f6d043c093ea49e569158e76ea48d7cd67c28
dua-shakeel <duashakeel125c@gmail.com>
2024-07-19 19:18:04 +0500

    Update available.php

--- available.php
+++ available.php
@@ -3,7 +3,7 @@
 <body>
 <?php
 	$database_host = "localhost";
-	$database_user = "DESKTOP-IPPHTT1\MSSQLEXPRESS";
+	$database_user = "192.168.10.4";
 	$database_pass = "pizza";
 	$database_name = "carrental";
 	$connection = mysqli_connect($database_host, $database_user, $database_pass, $database_name);
